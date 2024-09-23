f4cea63da563b5fa195a8fa2d017846990b7fe80
lab04 <lab04@lab4-08>
2024-09-23 20:41:33 -0300

    criação de campos na tela inicial, conexão com BD

--- src/usa/com/nba/atletas/operacoesController.java
+++ src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,19 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	
+	@FXML
+	private TextField txfNome;
+	@FXML
+	private TextField txfSobrenome;
+	@FXML
+	private TextField txfIdade;
+	@FXML
+	private TextField txfposicao;
+	@FXML
+	private TextField txfEquipe;
 	
 }
